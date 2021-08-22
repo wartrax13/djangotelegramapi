a578ed372016f5e8df073acbbedb498ae30e8dcc
wartrax13 <pedro.moisesandrade@gmail.com>
2021-08-22 16:33:09 -0300

    Apps, urls e implementada algumas views. close #2

--- bot/views.py
+++ bot/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return HttpResponse("Hello, world. This is the bot app.")
+
+def prova(request):
+    return HttpResponse("Isso é uma herança no django")
 
 # Create your views here.
