b7cce9e1d5209e8c3be415d010370c653289fb0d
wartrax13 <pedro.moisesandrade@gmail.com>
2021-08-22 17:20:30 -0300

    Configurando adm: user e mensagens
    close #3

--- bot/models.py
+++ bot/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class User(models.Model):
+    user_id = models.IntegerField(unique=True, primary_key=True)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+class Message(models.Model):
+    update_id = models.IntegerField(unique=True)
+    text = models.TextField(max_length=4096)
+    date = models.DateTimeField(default=timezone.now)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.text}'
